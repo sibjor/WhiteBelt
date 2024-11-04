d7d810c5e39d5e4b77876b7c87e643f01f05fcc0
sibjor <sixten.bjorling@student.forsbergsskola.se>
2024-11-04 11:35:01 +0100

    Update Program.cs

--- Kata1/Program.cs
+++ Kata1/Program.cs
@@ -6,15 +6,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        /*
-Prompt the player to enter their name.
-Read the player’s name from the console.
-Greet the player using their name, displaying a personalized message.
-Ask the player about their excitement level (from 1 to 10).
-Read the excitement level and display a response based on the value:
-7 or higher: Display an enthusiastic response.
-Lower than 7: Display a message encouraging the player.
-         */
         Console.WriteLine("Please provide your name: ");
         string answerName = Console.ReadLine();
         Console.WriteLine($"Hello {answerName.ToUpper()}! Nice to have you here");
