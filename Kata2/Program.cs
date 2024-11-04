6f2068cc104ba42322fbb5c1f92e343103442d2b
sibjor <sixten.bjorling@student.forsbergsskola.se>
2024-11-04 11:34:36 +0100

    Kata 2

--- Kata2/Program.cs
+++ Kata2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Character myCharacter = new("Arin", 100, 15.5f, true, 24.75);
+    }
+
+
+    class Character
+    {
+        public Character
+        ( // Parameters below
+            string name,
+            int healthPoints,
+            float attackPower,
+            bool isParalyzed,
+            double currencyAmount
+        )
+        {
+            // Constructor
+            _name = name;
+            _healthPoints = healthPoints;
+            _attackPower = attackPower;
+            _isParalyzed = isParalyzed;
+            _currencyAmount = currencyAmount;
+
+            PrintCharacterInfo();
+        }
+
+        public void PrintCharacterInfo()
+        {
+            Console.WriteLine($"Character name: {_name}" +
+                              $"\n Health Points: {_healthPoints}" +
+                              $"\n Attack Power: {_attackPower}" +
+                              $"\n IsParalyzed: {_isParalyzed}" +
+                              $"\n Gold Amount: ${_currencyAmount}");
+        }
+
+        string _name;
+        int _healthPoints;
+        float _attackPower;
+        bool _isParalyzed;
+        double _currencyAmount;
     }
 }
