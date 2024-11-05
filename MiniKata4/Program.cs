53aa4b876e9d6307b355e9cffc8829248fd642ac
sibjor <sixten.bjorling@student.forsbergsskola.se>
2024-11-05 11:29:26 +0100

    MiniKata4

---
+++ MiniKata4/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace MiniKata4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int enemyCount = 1;
+        
+        Console.WriteLine("This is a for loop: ");
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.WriteLine($"Enemy {i} spawned");
+        }
+        
+        Console.WriteLine("This is a while loop: ");
+
+        while (enemyCount <= 5)
+        {
+            Console.WriteLine($"Enemy {enemyCount} spawned");
+            enemyCount++;
+        }
+    }
+}
