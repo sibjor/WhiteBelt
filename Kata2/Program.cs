83ca956d58a6b4c783c02a4179eedfdee24edf17
sibjor <sixten.bjorling@student.forsbergsskola.se>
2024-11-05 11:18:18 +0100

    Kata2(1)
    Seems like some backend .Net setting is still not renamed to minikata

---
+++ Kata2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Kata2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string name = "Player";
+        int level = 1;
+        int levelUpgrade = 100;
+        float healthPoints = 5.5f;
+        double experiencePoints = 55.45;
+        bool magicAbility = true;
+        char rank = 'C';
+        
+        Console.WriteLine($"Name: {name}\nLevel: {level}\nHealthPoints: {healthPoints}\nXP: {experiencePoints}\nMagicAbility: {magicAbility}\nRank: {rank}" );
+        
+        if (experiencePoints == levelUpgrade)
+        {
+            level++;
+        }
+        Console.WriteLine($"Points needed to reach next level: {levelUpgrade-experiencePoints}");
+    }
+}
