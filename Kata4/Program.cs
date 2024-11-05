943ffa470b0b7d917a899099e77220d18968705b
sibjor <sixten.bjorling@student.forsbergsskola.se>
2024-11-05 11:49:44 +0100

    Kata4

---
+++ Kata4/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Kata4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int enemyCount = 5;
+        int waveCount = 2;
+
+        for (int i = 1; i <= waveCount; i++)
+        {
+            Console.WriteLine($"Wave #{i} starting...");
+
+            for (int o = 0; o < enemyCount; o++)
+            {
+                Console.WriteLine($"Enemy #{o + 1} entering the arena");
+            }
+        }
+        Console.WriteLine("All waves completed! Victory is yours!");
+    }
+}
