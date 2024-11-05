e89f6794676fd5c8302c6cbce983efbd5f2a3ca6
sibjor <sixten.bjorling@student.forsbergsskola.se>
2024-11-05 09:47:14 +0100

    MiniKata3
    After renaming to MiniKataX the option to "Run Kata..." is displaying the wrong program naming for several programs

--- MiniKata3/Program.cs
+++ MiniKata3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int playerLuck = 5;
+        Console.WriteLine("You encounter an enemy! Do you want to attempt an attack? (y/n)");
+        if (Console.ReadLine() == "y")
+        {
+            if (playerLuck > 7)
+            {
+                Console.WriteLine("Attack successful");
+            }
+            else if (playerLuck == 6 || playerLuck == 7)
+            {
+                Console.WriteLine("Attack barely successful");
+            }
+            else if (playerLuck < 6)
+            {
+                Console.WriteLine("Attack missed");
+            }
+        }
+        else if (Console.ReadLine() == "n")
+        {
+            Console.WriteLine("The player wished not to attack...");
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Try again.");
+        }
     }
 }
